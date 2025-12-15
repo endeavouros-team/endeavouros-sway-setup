e2c8b14a152c6243d687ae22822f1504999705fa
joekamprad <joekamprad@noreply.codeberg.org>
2025-12-15 16:41:48 +0100

    adding manual install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Maintainer: joekamprad [joekamprad //a_t// endeavouros.com]
+# run this from installed Archbased as normal user (as a script or line by line)
+git clone https://codeberg.org/joekamprad/endeavouros-sway-setup.git
+cd endeavouros-sway-setup/etc/skel/
+cp -R .config .local .icons $HOME/
+cp  .bashrc .gtkrc-2.0 packages-AUR.txt $HOME/
+# make scripts executable
+chmod -R +x $HOME/.config/sway/scripts
+pacman -S --needed --noconfirm - < packages-repository.txt 
+cd /
+rm -R endeavouros-sway-setup
+# start DM (ly)
+systemctl enable ly.service
+# disable TTY2 to be free for ly
+systemctl disable getty@tty2.service
+# needed for swayosd
+systemctl enable swayosd-libinput-backend.service
+echo "sway is now setup for your current user, you may reboot to login to sway"
