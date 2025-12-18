3b8ed623024b66aa6cb52ea266bce35f74c6e4af
joekamprad <joekamprad@noreply.codeberg.org>
2025-12-18 21:41:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ cd endeavouros-sway-setup/etc/skel/
 cp -R .config .local .icons $HOME/
 cp  .bashrc .gtkrc-2.0 packages-AUR.txt $HOME/
 git clone https://github.com/killajoe/sway_tools.git
-cp sway_tools/gracefully_exit/gracefully_exit $HOME/.config/sway/scripts/
 cp sway_tools/new_workspace/new_workspace $HOME/.config/sway/scripts/
-cp sway_tools/powermenu/powermenu $HOME/.config/sway/scripts/
+# using gracefully powermenu needs gracefully_exit script
+cp sway_tools/powermenu/powermenu_gracefully $HOME/.config/sway/scripts/powermenu
+cp sway_tools/gracefully_exit/gracefully_exit $HOME/.config/sway/scripts/
 cp sway_tools/power_profiles_switcher/power_profiles_switcher $HOME/.config/sway/scripts/
 cp sway_tools/swaycast/swaycast $HOME/.config/sway/scripts/
 cp sway_tools/swayshot/swayshot $HOME/.config/sway/scripts/
