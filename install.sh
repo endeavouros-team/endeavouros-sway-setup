046639eed7c3618057759fe140888c545d48d7a9
joekamprad <joekamprad@noreply.codeberg.org>
2025-12-18 11:59:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,15 @@ git clone https://codeberg.org/joekamprad/endeavouros-sway-setup.git
 cd endeavouros-sway-setup/etc/skel/
 cp -R .config .local .icons $HOME/
 cp  .bashrc .gtkrc-2.0 packages-AUR.txt $HOME/
+git clone https://github.com/killajoe/sway_tools.git
+cp sway_tools/gracefully_exit/gracefully_exit $HOME/.config/sway/scripts/
+cp sway_tools/new_workspace/new_workspace $HOME/.config/sway/scripts/
+cp sway_tools/powermenu/powermenu $HOME/.config/sway/scripts/
+cp sway_tools/power_profiles_switcher/power_profiles_switcher $HOME/.config/sway/scripts/
+cp sway_tools/swaycast/swaycast $HOME/.config/sway/scripts/
+cp sway_tools/swayshot/swayshot $HOME/.config/sway/scripts/
+cp sway_tools/waybar_toggle/waybar_toggle $HOME/.config/sway/scripts/
+cp sway_tools/weather/openweather $HOME/.config/sway/scripts/
 # make scripts executable
 chmod -R +x $HOME/.config/sway/scripts
 sudo pacman -S --needed --noconfirm - < packages-repository.txt 
